fdeee466615ab2f1553af2ffa45b39905b6136a6
Khalid <alhajr20@gmail.com>
2021-06-02 15:10:55 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
 window.addEventListener('DOMContentLoaded', () => {
-    /* ... */
+    const menu = document.querySelector('.header__nav'),
+          openBtn = document.querySelector('.header__menu .open'),
+          closeBtn = document.querySelector('.header__nav .close');
+
+    openBtn.addEventListener('click', () => {
+        menu.classList.add('nav_active');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        menu.classList.remove('nav_active');
+    });
+
+    let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.3;
+    
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+
+                start = null;
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock));
+
+                    document.documentElement.scrollTo(0, r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        });
+    });
 });
